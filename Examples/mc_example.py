09d03da8fc0eae860798aa5422f628d66d729aea
Kingcong123 <congw0609@gmail.com>
2025-09-09 21:38:41 -0400

    Update

--- Examples/mc_example.py
+++ Examples/mc_example.py
@@ -1,11 +1,11 @@
 from monte_carlo.MCsolo import monte_carlo_stock_paths
 
 def main():
-    S, mu, T, r, sigma = 100, 0.05, 1, 0.05, 0.2
+    S, mu, T, sigma = 100, 0.05, 1, 0.05, 0.2
     steps, num_paths = 252, 10000
     plot_paths = 20
 
-    mean = monte_carlo_stock_paths(S, mu, T, sigma, steps, num_paths, 20)
+    mean = monte_carlo_stock_paths(S, mu, T, sigma, steps, num_paths, plot_paths)
 
     print(f"Monte Carlo Simulated Mean: {mean}")
 
